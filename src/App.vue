638674fac6ecb4a5affbb1ec871fcc10e8eaf379
Marcos M. López <marcoslopez1895@gmail.com>
2024-05-20 15:05:39 -0400

    init

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <NavbarComponent />
-  <div class="max-w-[1400px] mx-auto">
-    <router-view></router-view>
-  </div>
-</template>
-<script setup lang="ts">
-import NavbarComponent from './components/NavbarComponent.vue';
-</script>
